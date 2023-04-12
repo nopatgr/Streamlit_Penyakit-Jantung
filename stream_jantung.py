ac2916b7911150213fa334ec763b9fbc38bd0b51
nopatgr <101387563+nopatgr@users.noreply.github.com>
2023-04-12 13:22:24 +0700

    Update stream_jantung.py

--- stream_jantung.py
+++ stream_jantung.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd 
-from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 
 # Load data
